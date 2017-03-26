de6ab13a0295b17875622c70ab7d524f706b9256
SergioC <sergio.carbone@gmail.com>
2017-03-26 13:05:09 -0300

    .

--- TP_1_Cascara/TP_1_Cascara/funciones.h
+++ TP_1_Cascara/TP_1_Cascara/funciones.h
@@ -1,3 +1,16 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+
+
+
+#endif // FUNCIONES_H_INCLUDED
+float dividir ( float a, float b);
+    float suma ( float a, float b);
+    float resta( float a, float b);
+    float multiplicar(float a, float b);
+    int factorial (int enteroA);
+
+
 float multiplicar(float a, float b)
 {
     float total;
@@ -54,6 +67,3 @@ int factorial (enteroA)
     }
 
 }
-
-
-
